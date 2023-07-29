ff71d2547909f114d93652a1f6c9ff20e40806ad
tgasque <tiagogasque@outlook.com>
2023-07-29 10:27:48 -0300

    JS

---
+++ JS/Variaveis.js
@@ -0,0 +1,11 @@
+/*
+Variáveis, é um pedacinho de memoria que separamos dentro do nosso computador, damos um nome para ele e podemos colocar as informações dentro
+
+    -let - Pode alterar o Valor quando eu quiser
+    -const - Valor Constante "O Valor não pode ser declarado novamente"
+    -var - Descontinuado 
+*/ 
+
+let abacate = "40"
+
+console.log (abacate)
