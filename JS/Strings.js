212aee84e23eb41f0c0256e498c5d51504507e4c
tgasque <tiagogasque@outlook.com>
2023-07-29 10:27:26 -0300

    JS

---
+++ JS/Strings.js
@@ -0,0 +1,7 @@
+/*
+1- String - texto. temos 3 formas de quardar textos usando o as aspas 
+ex: "textos com aspas duplas"
+'aspas simpres'
+`crase posso pular linhas que com as aspas não consigo` ou Template Literals ou Template String e nele poremos pular linha e criar variaveis dentro dele
+e podemos usar também as variaves
+ */
---
+++ JS/tipos de dados.js
@@ -0,0 +1,18 @@
+/*
+Tipos de dados
+1. Strings -> Textos 
+2. Numbers -> Números
+3. Boolean -> Boleano
+4. Object-> cada objeto tem seu nome e valor  
+5. Null & Undefined -> Augencia de informação porem tem diferença o null nos definimos porem Undefined é quando fazemos alguma cagada no codgo 
+6. Array / Vetor / Matriz : São estrutura de dados-> podemos guardar varias coisa dentro da estrutura.
+7. if -> (Se)  else -> (Se não)
+8. Funtions -> Funçoes (Trecho de codigo que é executado SOMENTE qundo solicitado "PARENTE GENTE BOA")
+9. DOM -> a Arvore de elementos da pagina HTML e o java script consegue manipular a DOM
+*/
+
+const Numbers1 = 32 * 2
+const fakeNuber =  "32 / 2"
+
+console.log (Numbers1)
+console.log (fakeNuber) 
