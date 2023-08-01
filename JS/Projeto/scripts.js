393c6aae8e597bdb53e4d31bc01e5a9c06ad5a6f
tgasque <tiagogasque@outlook.com>
2023-08-01 20:26:56 -0300

    update

---
+++ JS/Projeto/scripts.js
@@ -0,0 +1,7 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    console.log("Funcionou")
+}
+
+convertButton.addEventListener("click", convertValues)
