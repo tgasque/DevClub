7f0df9a0d2a4e25da3dc4fcfcae3a715c8fd51d0
tgasque <tiagogasque@outlook.com>
2023-08-02 17:26:28 -0300

    JS

--- JS/Projeto/scripts.js
+++ JS/Projeto/scripts.js
@@ -1,7 +1,34 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues(){
-    console.log("Funcionou")
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector (".currency-value-to-convert") //Valor em real
+    const currencyValueConverted = document.querySelector (".currency-value-converted") //outras Moedas
+  
+console.log(currencySelect.value)
+
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+    if(currencySelect.value == "dolar"){ //Se o Select estiver selecionado o Valor de Dolar entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style:"currency",
+            currency:"USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if(currencySelect.value == "euro"){ //Se o Select estiver selecionado o Valor de Euro entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style:"currency",
+            currency:"EUR"
+        }).format(inputCurrencyValue/euroToday)
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style:"currency",
+        currency:"BRL"
+    }).format(inputCurrencyValue)  
 }
 
 convertButton.addEventListener("click", convertValues)
